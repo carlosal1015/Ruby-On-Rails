e096da910777d2726c4e21a1b9c363fda4754c0d
Oromion <caznaranl@uni.pe>
2018-12-29 21:26:58 -0500

    Update

---
+++ hello-world.rb
@@ -0,0 +1,12 @@
+# Program for displaying "Hello World!"
+# by Oromion
+puts 'Hello World!' # Example Comment
+puts
+puts 'zzz'
+print 'Hello Worldo!'
+
+puts '##########'
+puts 
+puts '#1#2#3#4#5#6'
+print
+puts 'zzz'
---
+++ hello-worldx3e.rb
@@ -0,0 +1,7 @@
+class This_and_that
+	def three_times
+		puts 'Hello World!'
+		puts 'Hello World!'
+		puts 'Hello Qorld!'
+	end
+end
